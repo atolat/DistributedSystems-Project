18ea37a233049d2f6eb51e35938c4508d50a473b
atolat <atolat@uci.edu>
2016-05-23 13:28:49 -0700

    mongoose-mongo config

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
-var bodyParser= require('body-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var gcm=require('node-gcm');
 var routes = require('./routes/routes')(app);
 var PORT = process.env.PORT || 3000;
 
+//Establish DB connection
+mongoose.connect('mongodb://localhost/node-gcm');
 
 //GCM integration
 var message = new gcm.Message();
@@ -20,6 +23,47 @@ app.use(bodyParser.json());
  app.get('/',function(req, res){
     res.send('API Root');
     });
+
+
+
+//Mongo Scratchpad
+var User = require('./models/user');
+
+var arpan=new User({
+    username: 'Arpan',
+    password: 'letmein'
+});
+
+arpan.save(function(err){
+    if(err) throw err;
+    
+    console.log('User saved!');
+});
+
+
+User.find({}, function(err, users){
+    if(err) throw err;
+    
+    console.log(users);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT,function(){
 console.log('Express listening on port:: '+PORT);
