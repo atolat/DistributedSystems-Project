4f6a18da8d9b4e18fb8684f31fb58a1a856853ad
atolat <atolat@uci.edu>
2016-05-24 03:49:54 -0700

    Added Twilio client

--- routes/scratchpad-server.js
+++ routes/scratchpad-server.js
@@ -77,8 +77,8 @@
             //Send Twilio message on POST
             client.sendMessage({
 
-                to: '+19492937594', // Any number Twilio can deliver to
-                from: '+19493000798', // A number you bought from Twilio and can use for outbound communication
+                to: '+19493000798', // Any number Twilio can deliver to
+                from: '+19492200716', // A number you bought from Twilio and can use for outbound communication
                 body: 'Hello from node!' // body of the SMS message
 
             }, function (err, responseData) { //this function is executed when a response is received from Twilio
