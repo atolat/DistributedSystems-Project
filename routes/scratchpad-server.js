aedfcc3c785f8782b80e01db2091a22c77e5e34f
atolat <atolat@uci.edu>
2016-05-24 03:11:59 -0700

    test

--- routes/scratchpad-server.js
+++ routes/scratchpad-server.js
@@ -16,6 +16,11 @@ var message = {
     'data.key1': 'value1',
     'data.key2': 'value2'
 };
+     
+     //Twilio Integration
+     var client = require('twilio')('ACd54cb6f1b8a8bf9d23fe511d24d3459e', '472205f35904bda6943ed88a1343e2b1');
+
+     
 
     
  app.get('/',function(req, res){
@@ -66,7 +71,27 @@ var message = {
         console.log("Sent with message ID: ", messageId);
     }
 });
-        
+        //Twilio message
+        client.sendMessage({
+
+    to:'+19493000798', // Any number Twilio can deliver to
+    from: '+19492200716', // A number you bought from Twilio and can use for outbound communication
+    body: 'Hello from Twilio' // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) { // "err" is an error received during the request, if any
+
+        // "responseData" is a JavaScript object containing data received from Twilio.
+        // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+        // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+        console.log(responseData.from); // outputs "+14506667788"
+        console.log(responseData.body); // outputs "word to your mother."
+
+    }
+});
+
 
         res.json(body);
 
