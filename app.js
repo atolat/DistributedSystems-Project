1b52dd4655eb99f342c633283e4aa48c935517d7
atolat <atolat@uci.edu>
2016-05-24 02:21:22 -0700

    test

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var routes = require('./routes/scratchpad-server')(app);
 
-
+ app.get('/',function(req, res){
+        res.send('Todo API Root');
+    });
 
 
 app.listen(PORT,function(){
