32413300cf2863510f02772a60a591ee00fbf504
atolat <atolat@uci.edu>
2016-05-24 18:03:42 -0700

    Added Twilio client

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var app = express();
 var routes = require('./routes/scratchpad-server')(app);
 var PORT = process.env.PORT || 3000;
 
+allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
 
 
 
