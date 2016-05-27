d75fc5cc1f5c966739b999fbb234eaeb4c8ff56f
Arpan Tolat <Arpan Tolat>
2016-05-27 15:54:08 -0700

    Testing

--- routes/final-server.js
+++ routes/final-server.js
@@ -114,7 +114,7 @@ module.exports = function (app) {
     
     //Wifi off
     //url/wifi
-    app.post('/wifi', function (req, res) {
+    app.post('/sms', function (req, res) {
         var body = _.pick(req.body, 'name', 'token','num', 'wifi');
        
         
